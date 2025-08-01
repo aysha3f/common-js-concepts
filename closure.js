63e0fdb89612f1f12fe4408db2f15b9b7056dcce
aysha3f <tahiaysha220@gmail.com>
2025-08-01 10:24:21 +0600

    what is closure and how to use it learn

---
+++ closure.js
@@ -0,0 +1,17 @@
+// node closure.js
+function kitchen (){
+let roast = 0;
+return function(){
+    roast ++;
+    return roast;
+}
+}
+const firstServer = kitchen();
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
