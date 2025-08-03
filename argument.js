483993aa206c78e8cd99adf49c3d801b5ce6a85b
aysha3f <tahiaysha220@gmail.com>
2025-08-03 23:59:36 +0600

    argument

--- argument.js
+++ argument.js
@@ -1,1 +1,12 @@
 // node argument.js
+// array like object 
+function sum (a,b,c){
+ //  console.log( typeof arguments);
+ const args = [...arguments];
+ // console.log(args);
+    const result = a + b + c;
+    return result;
+}
+const total = sum(45, 67, 45,98, 56);
+//console.log(total);
+console.log(sum.length)
